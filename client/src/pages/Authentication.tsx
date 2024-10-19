7b44a99a45f7fd76d49addca9b67ea4e08462a6f
Lamia Hossain <lamiahossain2017@gmail.com>
2024-10-20 00:29:24 +0600

    Authentication Page UI - ongoing

--- client/src/pages/Authentication.tsx
+++ client/src/pages/Authentication.tsx
@@ -1,5 +1,16 @@
+import { Player } from "@lottiefiles/react-lottie-player";
+
 const Authentication = () => {
-  return <div className="">Authentication</div>;
+  return (
+    <div className="">
+      Authentication
+      <Player
+        autoplay
+        loop
+        src="https://lottie.host/5305d59e-b867-4ec4-9854-78a935fd7d98/PPkQUPIv8Z.json"
+      />
+    </div>
+  );
 };
 
 export default Authentication;
